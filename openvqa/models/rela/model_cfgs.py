0aad87ac4f04c75ee448a707794454a56ad55d28
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2020-11-20 16:45:07 +1100

    Update model_cfgs.py

--- openvqa/models/rela/model_cfgs.py
+++ openvqa/models/rela/model_cfgs.py
@@ -11,7 +11,7 @@ class Cfgs(BaseCfgs):
         super(Cfgs, self).__init__()
 
         self.LAYER = 6
-        self.HIDDEN_SIZE = 512
+        self.HIDDEN_SIZE = 1024
         self.BBOXFEAT_EMB_SIZE = 2048
         self.FF_SIZE = 2048
         self.MULTI_HEAD = 8
