e8560a4a4a087ddbc28e998ba49c3297179cc744
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2020-11-24 03:03:03 +1100

    Update net.py

--- openvqa/models/rela/net.py
+++ openvqa/models/rela/net.py
@@ -110,8 +110,8 @@ class Net(nn.Module):
         lang_feat = self.embedding(ques_ix)
         #self.lstm.flatten_parameters()
         #lang_feat, _ = self.lstm(lang_feat)
-        position_embed = self.pos_embedding(torch.arange(ques_ix.shape[1], device='cuda').repeat(ques_ix.shape[0], 1))
-        postag_embed = self.tag_embedding(ques_postag)
+        position_embed = self.position_embedding(torch.arange(ques_ix.shape[1], device='cuda').repeat(ques_ix.shape[0], 1))
+        postag_embed = self.postag_embedding(ques_postag)
         lang_feat = lang_feat + position_embed + postag_embed
         img_feat, img_feat_mask, bbox_feat = self.adapter(frcn_feat, grid_feat, bbox_feat)
 
