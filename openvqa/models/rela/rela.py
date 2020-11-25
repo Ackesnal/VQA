a04c2189e1d84a8dd3ed461dbb247cfb113431c4
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2020-11-25 16:26:27 +1100

    Update rela.py

--- openvqa/models/rela/rela.py
+++ openvqa/models/rela/rela.py
@@ -216,24 +216,18 @@ class SGA(nn.Module):
 
         self.dropout3 = nn.Dropout(__C.DROPOUT_R)
         self.norm3 = LayerNorm(__C.HIDDEN_SIZE)
-        
-        self.relu = nn.ReLU()
-        self.linear = nn.Linear(14, 100)
-        
+    
     def forward(self, x, y, x_mask, y_mask):
         
         x = self.norm1(x + self.dropout1(
             self.mhatt1(v=x, k=x, q=x, mask=x_mask)
         ))
-        
-        t = self.dropout2(self.mhatt2(v=x, k=x, q=y, mask=x_mask))
-        t = self.relu(self.linear(t.reshape(t.shape[0], t.shape[2], t.shape[1])))
-        x = self.norm2(x + t.reshape(t.shape[0], t.shape[2], t.shape[1]))
-        
+        x = self.norm2(x + self.dropout2(
+            self.mhatt2(v=y, k=y, q=x, mask=y_mask)
+        ))
         x = self.norm3(x + self.dropout3(
             self.ffn(x)
         ))
-
         return x
 
 
